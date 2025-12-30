2d5bc1481236e68b359fec9e589d611deae20760
Senthie <seemoon2077@gmail.com>
2025-12-30 09:12:10 +0800

    清空导入

--- api/app/engine/nodes/__init__.py
+++ api/app/engine/nodes/__init__.py
@@ -6,22 +6,22 @@ Each node type is automatically registered when imported due to the @register_no
 """
 
 # Import all node executors to register them
-from app.engine.nodes.code_node import CodeNodeExecutor
-from app.engine.nodes.condition_node import ConditionNodeExecutor
-from app.engine.nodes.http_node import HTTPNodeExecutor
-from app.engine.nodes.llm_node import LLMNodeExecutor
-from app.engine.nodes.ollama_node import OllamaNodeExecutor
-from app.engine.nodes.transform_node import TransformNodeExecutor
+# from app.engine.nodes.code_node import CodeNodeExecutor
+# from app.engine.nodes.condition_node import ConditionNodeExecutor
+# from app.engine.nodes.http_node import HTTPNodeExecutor
+# from app.engine.nodes.llm_node import LLMNodeExecutor
+# from app.engine.nodes.ollama_node import OllamaNodeExecutor
+# from app.engine.nodes.transform_node import TransformNodeExecutor
 
-# Export the node executors for external use
-__all__ = [
-    'LLMNodeExecutor',
-    'OllamaNodeExecutor',
-    'ConditionNodeExecutor',
-    'CodeNodeExecutor',
-    'HTTPNodeExecutor',
-    'TransformNodeExecutor',
-]
+# # Export the node executors for external use
+# __all__ = [
+#     'LLMNodeExecutor',
+#     'OllamaNodeExecutor',
+#     'ConditionNodeExecutor',
+#     'CodeNodeExecutor',
+#     'HTTPNodeExecutor',
+#     'TransformNodeExecutor',
+# ]
 
 # The node executors are automatically registered with the global registry
 # when this module is imported due to the @register_node_executor decorators
