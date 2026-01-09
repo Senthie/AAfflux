ca72374841db1e7d0f072d71f03372254e731c07
Senthie <seemoon2077@gmail.com>
2026-01-09 15:45:58 +0800

    添加获取获取当前用户的租户信息

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -2,41 +2,30 @@
  * @Author: Senthie seemoon2077@gmail.com
  * @Date: 2025-11-26 10:24:58
  * @LastEditors: Senthie seemoon2077@gmail.com
- * @LastEditTime: 2026-01-07 15:31:11
+ * @LastEditTime: 2026-01-09 15:02:56
  * @FilePath: /web/src/pages/IndexPage.vue
  * @Description: 用户登陆后的主页
  *
  * Copyright (c) 2026 by Senthie email: seemoon2077@gmail.com, All Rights Reserved.
 -->
 <script setup lang="ts">
-import { ref } from 'vue'
-import type { Todo, Meta } from 'components/models'
+import { v1_get_workspaces } from 'src/apis/workscpace_api'
+import type { IWorkspaceResponse } from 'src/interfaces/IWorkspace'
+import { onMounted, ref } from 'vue'
 
-const todos = ref<Todo[]>([
-    {
-        id: 1,
-        content: 'ct1',
-    },
-    {
-        id: 2,
-        content: 'ct2',
-    },
-    {
-        id: 3,
-        content: 'ct3',
-    },
-    {
-        id: 4,
-        content: 'ct4',
-    },
-    {
-        id: 5,
-        content: 'ct5',
-    },
-])
+const workspaces = ref<Array<IWorkspaceResponse>>([])
+
+const handle_get_workspaces = async () => {
+    const res = await v1_get_workspaces()
+    return res.data
+}
+
+onMounted(async () => {
+    workspaces.value = await handle_get_workspaces()
+})
 </script>
 <template>
     <q-page class="row justify-evenly">
-        <div></div>
+        <div>{{ workspaces }}</div>
     </q-page>
 </template>
