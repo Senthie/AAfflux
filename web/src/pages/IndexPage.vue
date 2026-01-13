42a40404b765b1299d0315e80be45865bda73146
Senthie <seemoon2077@gmail.com>
2026-01-13 12:11:32 +0800

    引入 create_worrkflow 组件

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -2,30 +2,27 @@
  * @Author: Senthie seemoon2077@gmail.com
  * @Date: 2025-11-26 10:24:58
  * @LastEditors: Senthie seemoon2077@gmail.com
- * @LastEditTime: 2026-01-09 15:02:56
+ * @LastEditTime: 2026-01-12 11:36:58
  * @FilePath: /web/src/pages/IndexPage.vue
  * @Description: 用户登陆后的主页
  *
  * Copyright (c) 2026 by Senthie email: seemoon2077@gmail.com, All Rights Reserved.
 -->
 <script setup lang="ts">
-import { v1_get_workspaces } from 'src/apis/workscpace_api'
-import type { IWorkspaceResponse } from 'src/interfaces/IWorkspace'
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
+import CreateWorkflowCp from 'src/components/workflows/CreateWorkflowCp.vue'
+import { useWorkspaceStore } from 'src/stores/workspace-store'
+const workspace_store = useWorkspaceStore()
 
-const workspaces = ref<Array<IWorkspaceResponse>>([])
-
-const handle_get_workspaces = async () => {
-    const res = await v1_get_workspaces()
-    return res.data
-}
-
-onMounted(async () => {
-    workspaces.value = await handle_get_workspaces()
+onMounted(() => {
+    void workspace_store.handle_get_workspaces()
 })
 </script>
 <template>
     <q-page class="row justify-evenly">
-        <div>{{ workspaces }}</div>
+        <div>{{ workspace_store.workspace }}</div>
+        <div>
+            <CreateWorkflowCp />
+        </div>
     </q-page>
 </template>
