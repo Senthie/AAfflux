aca4db227f4097aff84de56a347ec9b0f7f32bca
Senthie <seemoon2077@gmail.com>
2026-01-16 15:12:17 +0800

    实现了右键添加节点的方法

--- web/src/pages/workflows/MainPage.vue
+++ web/src/pages/workflows/MainPage.vue
@@ -2,7 +2,7 @@
  * @Author: Senthie seemoon2077@gmail.com
  * @Date: 2026-01-14 15:07:09
  * @LastEditors: Senthie seemoon2077@gmail.com
- * @LastEditTime: 2026-01-15 12:18:30
+ * @LastEditTime: 2026-01-15 15:16:26
  * @FilePath: /web/src/pages/workflows/MainPage.vue
  * @Description:
  *
@@ -10,6 +10,7 @@
 -->
 <script setup lang="ts">
 import { onBeforeUnmount, onMounted } from 'vue'
+import type { IUI } from 'leafer-ui'
 import { App, Rect } from 'leafer-ui'
 import { DotMatrix } from 'leafer-x-dotwuxian'
 // import { useWindowSize } from '@vueuse/core'
@@ -18,8 +19,9 @@ import { Connector } from 'leafer-connector'
 import ContextMenu from '@imengyu/vue3-context-menu'
 import '@leafer-in/editor' // 导入图形编辑器插件
 import '@leafer-in/viewport' // 导入视口插件 (可选)
+import '@leafer-in/find' // 导入查找元素插件
 
-let app: App | null = null
+let app: App = null as unknown as App
 
 const onContextMenu = (e: MouseEvent) => {
     // 阻止浏览器默认菜单
@@ -33,31 +35,8 @@ const onContextMenu = (e: MouseEvent) => {
             {
                 label: '添加节点',
                 onClick: () => {
-                    createRect()
+                    createRect(e.x, e.y)
                 },
-            },
-            {
-                label: '编辑',
-                children: [
-                    {
-                        label: '复制',
-                        onClick: () => {
-                            console.log('复制')
-                        },
-                    },
-                    {
-                        label: '粘贴',
-                        onClick: () => {
-                            console.log('粘贴')
-                        },
-                    },
-                    {
-                        label: '删除',
-                        onClick: () => {
-                            console.log('删除')
-                        },
-                    },
-                ],
             },
             {
                 label: '视图',
@@ -79,6 +58,37 @@ const onContextMenu = (e: MouseEvent) => {
         ],
     })
 }
+
+const onNodeMenu = (e: MouseEvent, node: IUI) => {
+    // 阻止浏览器默认菜单
+    e.preventDefault()
+
+    // 显示自定义右键菜单
+    ContextMenu.showContextMenu({
+        x: e.x,
+        y: e.y,
+        items: [
+            {
+                label: '复制',
+                onClick: () => {
+                    console.log('复制')
+                },
+            },
+            {
+                label: '粘贴',
+                onClick: () => {
+                    console.log('粘贴')
+                },
+            },
+            {
+                label: '删除',
+                onClick: () => {
+                    node.remove()
+                },
+            },
+        ],
+    })
+}
 onMounted(() => {
     app = new App({
         view: window,
@@ -90,7 +100,12 @@ onMounted(() => {
 
     // 监听 leafer-ui 的右键事件
     app.on('pointer.menu', (e: { origin: MouseEvent }) => {
-        onContextMenu(e.origin)
+        if (app?.editor.single) {
+            const { element } = app.editor
+            onNodeMenu(e.origin, element as IUI)
+        } else {
+            onContextMenu(e.origin)
+        }
     })
     const dot = new DotMatrix(app, {
         dotColor: '#D2D4D7',
@@ -101,50 +116,68 @@ onMounted(() => {
     })
     dot.enableDotMatrix(true)
     const a = new Rect({
+        name: 'a',
         x: 100,
         y: 100,
+        editable: true,
         width: 200,
         height: 160,
         fill: '#32cd79',
         draggable: true,
     })
     const b = new Rect({
+        name: 'b',
         x: 520,
         y: 280,
+        editable: true,
         width: 220,
         height: 160,
         fill: '#3b82f6',
         draggable: true,
     })
     const c = new Rect({
+        name: 'c',
         x: 520,
-        y: 280,
+        y: 580,
+        editable: true,
         width: 220,
         height: 160,
-        fill: '#	#1E90FF',
+        fill: '#1E90FF',
         draggable: true,
+        cornerRadius: 20,
     })
     const edge = new Connector(app, { from: a, to: b, stroke: '#32cd79' })
     const edge2 = new Connector(app, { from: c, to: b, stroke: '#32cd79' })
     app.tree.add([a, b, c, edge, edge2])
 })
 
-const createRect = () => {
-    const c = new Rect({
-        x: 520,
-        y: 280,
-        width: 220,
-        height: 160,
+const createRect = (x: number, y: number) => {
+    let num = 0
+    // TOOD 这样创建 rect 并不严谨 应该通过 NODE表
+    // 继续去查询节点的名字是否存在重复
+    const rect = new Rect({
+        name: `rect${num}`,
+        x: x,
+        y: y,
+        editable: true,
+        width: 200,
+        height: 100,
         fill: '#1E90FF',
         draggable: true,
+        cornerRadius: 20,
     })
-    app?.tree.add(c)
+    do {
+        num += 1
+        rect.name = `rect${num}`
+    } while (num <= 3)
+
+    app.tree.add(rect)
 }
 
 onBeforeUnmount(() => {
     // leafer-editor 不同版本销毁方法名可能不同，这里尽量兜底
     app?.destroy?.()
-    app = null
+    app = null as unknown as App
 })
 </script>
 
