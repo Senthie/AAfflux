eb993e88c71159a8a128eaf059b93a97cf855391
Senthie <seemoon2077@gmail.com>
2026-01-19 17:09:02 +0800

    realize NodeRect show and test connetion

--- web/src/pages/workflows/MainPage.vue
+++ web/src/pages/workflows/MainPage.vue
@@ -2,14 +2,14 @@
  * @Author: Senthie seemoon2077@gmail.com
  * @Date: 2026-01-14 15:07:09
  * @LastEditors: Senthie seemoon2077@gmail.com
- * @LastEditTime: 2026-01-15 15:16:26
+ * @LastEditTime: 2026-01-19 17:02:53
  * @FilePath: /web/src/pages/workflows/MainPage.vue
  * @Description:
  *
  * Copyright (c) 2026 by Senthie email: seemoon2077@gmail.com, All Rights Reserved.
 -->
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted } from 'vue'
+import { onBeforeUnmount, onMounted, ref } from 'vue'
 import type { IUI } from 'leafer-ui'
 import { App, Rect } from 'leafer-ui'
 import { DotMatrix } from 'leafer-x-dotwuxian'
@@ -20,8 +20,33 @@ import ContextMenu from '@imengyu/vue3-context-menu'
 import '@leafer-in/editor' // 导入图形编辑器插件
 import '@leafer-in/viewport' // 导入视口插件 (可选)
 import '@leafer-in/find' // 导入查找元素插件
+import type { IPageReq, IPageRes } from 'src/interfaces/Ipage'
+import type { IPluginBase } from 'src/interfaces/IPlugin'
+import { v1_plugins_list } from 'src/apis/plugin_api'
+import { NodeRect } from 'src/utils/nodeReact'
 
 let app: App = null as unknown as App
+const add_node_visiable = ref(false)
+const page_res = ref<IPageRes<IPluginBase>>({
+    total: 0,
+    size: 10,
+    current: 1,
+    orders: [],
+    maxLimit: 100,
+    records: [],
+})
+
+const handle_get_plugins = async () => {
+    const page_req: IPageReq = {
+        total: page_res.value.total,
+        size: page_res.value.size,
+        current: page_res.value.current,
+        orders: [],
+        maxLimit: 0,
+    }
+    const res = await v1_plugins_list(page_req)
+    page_res.value.records = res.data.records
+}
 
 const onContextMenu = (e: MouseEvent) => {
     // 阻止浏览器默认菜单
@@ -35,7 +60,8 @@ const onContextMenu = (e: MouseEvent) => {
             {
                 label: '添加节点',
                 onClick: () => {
-                    createRect(e.x, e.y)
+                    add_node_visiable.value = !add_node_visiable.value
+                    // createRect(e.x, e.y)
                 },
             },
             {
@@ -115,40 +141,31 @@ onMounted(() => {
         minSize: 0.1,
     })
     dot.enableDotMatrix(true)
-    const a = new Rect({
-        name: 'a',
+    const a = new NodeRect({
+        title: 'a',
         x: 100,
         y: 100,
         editable: true,
-        width: 200,
-        height: 160,
-        fill: '#32cd79',
-        draggable: true,
     })
-    const b = new Rect({
-        name: 'b',
+    const b = new NodeRect({
+        title: 'b',
         x: 520,
         y: 280,
         editable: true,
-        width: 220,
-        height: 160,
-        fill: '#3b82f6',
-        draggable: true,
     })
-    const c = new Rect({
-        name: 'c',
+    const c = new NodeRect({
+        title: 'c',
+        icon: 'https://n8niostorageaccount.blob.core.windows.net/n8nio-strapi-blobs-prod/assets/Docu_Seal_b9ab4f1bfb.svg',
         x: 520,
         y: 580,
         editable: true,
-        width: 220,
-        height: 160,
-        fill: '#1E90FF',
-        draggable: true,
-        cornerRadius: 20,
     })
     const edge = new Connector(app, { from: a, to: b, stroke: '#32cd79' })
     const edge2 = new Connector(app, { from: c, to: b, stroke: '#32cd79' })
     app.tree.add([a, b, c, edge, edge2])
+
+    // 获取插件
+    void handle_get_plugins()
 })
 
 const createRect = (x: number, y: number) => {
@@ -182,8 +199,28 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div class="box">
-        <div ref="container"></div>
+    <div>
+        <template>
+            <q-dialog v-model="add_node_visiable">
+                <q-card style="min-width: 350px">
+                    <q-card-section>
+                        <div class="text-h6">创建节点</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        <template
+                            v-for="(plugin, index) in page_res.records"
+                            :key="index"
+                        >
+                            <q-btn :label="plugin.name" color="primary" />
+                        </template>
+                    </q-card-section>
+
+                    <q-card-actions align="right" class="text-primary">
+                    </q-card-actions>
+                </q-card>
+            </q-dialog>
+        </template>
     </div>
 </template>
 
