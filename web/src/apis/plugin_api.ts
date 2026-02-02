7beb350bb841525ef49dd697c5a8c196e1443d23
Senthie <seemoon2077@gmail.com>
2026-02-02 10:36:14 +0800

    add get plugin info

--- web/src/apis/plugin_api.ts
+++ web/src/apis/plugin_api.ts
@@ -2,7 +2,7 @@
  * @Author: Senthie seemoon2077@gmail.com
  * @Date: 2026-01-19 14:49:12
  * @LastEditors: Senthie seemoon2077@gmail.com
- * @LastEditTime: 2026-01-19 17:18:35
+ * @LastEditTime: 2026-01-30 15:13:29
  * @FilePath: /web/src/apis/plugin_api.ts
  * @Description: 请求 plugin 的接口 方法
  *
@@ -49,6 +49,39 @@ export async function v1_plugins_list(
             code: 500,
             msg: errorMessage,
             data: {} as IPageRes<PluginResponse>,
+            timestamp: new Date().toISOString(),
+        }
+    }
+}
+
+export async function v1_plugins_id(
+    id: string
+): Promise<IResponse<PluginResponse>> {
+    try {
+        const response = await api.get<IResponse<PluginResponse>>(
+            `/v1/plugins/${id}`
+        )
+
+        if (response.data.code !== 200) {
+            Notify.create({
+                type: 'negative',
+                message: response.data.msg || '获取插件详情失败',
+            })
+        }
+
+        return response.data
+    } catch (error) {
+        const errorMessage = extractErrorMessage(error)
+
+        Notify.create({
+            type: 'negative',
+            message: errorMessage,
+        })
+
+        return {
+            code: 500,
+            msg: errorMessage,
+            data: {} as PluginResponse,
             timestamp: new Date().toISOString(),
         }
     }
